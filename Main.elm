a011e3bac192b92b7bc1f4558f8bbaf5062fc7e9
James Deery <james.deery@jadex.org>
2017-04-06 20:53:11 +0100

    Flowing water

--- Main.elm
+++ Main.elm
@@ -5,16 +5,19 @@ import Html exposing (..)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Svg.Events exposing (..)
+import Time exposing (..)
+import Debug
 
 
 type alias Model =
-    { towers : Array Int
+    { towers : Array Float
     , buckets : Array Float
     }
 
 
 type Msg
     = Rain Int
+    | Tick
 
 
 init : ( Model, Cmd Msg )
@@ -29,20 +32,99 @@ init =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Rain i ->
-            case Array.get i model.buckets of
-                Just h ->
-                    ( { model | buckets = Array.set i (h + 0.2) model.buckets }
-                    , Cmd.none
-                    )
+        Tick ->
+            ( flow model, Cmd.none )
 
-                Nothing ->
-                    ( model, Cmd.none )
+        Rain i ->
+            ( fillBucket i 0.2 model, Cmd.none )
+
+
+fillBucket : Int -> Float -> Model -> Model
+fillBucket i amount model =
+    case Array.get i model.buckets of
+        Just h ->
+            { model | buckets = Array.set i (h + amount) model.buckets }
+
+        Nothing ->
+            model
+
+
+flow : Model -> Model
+flow model =
+    List.foldl flowBucket
+        model
+        (List.range 0 (Array.length model.buckets))
+
+
+flowBucket : Int -> Model -> Model
+flowBucket i model =
+    case ( getHeights (i - 1) model, getHeights i model, getHeights (i + 1) model ) of
+        ( Just ( lt, lb ), Just ( t, b ), Just ( rt, rb ) ) ->
+            if b < 0.2 then
+                model
+            else
+                let
+                    lAmount =
+                        if t + b > lt + lb then
+                            0.1
+                        else
+                            0
+
+                    rAmount =
+                        if t + b > rt + rb then
+                            0.1
+                        else
+                            0
+                in
+                    fillBucket i -(lAmount + rAmount) model
+                        |> fillBucket (i - 1) lAmount
+                        |> fillBucket (i + 1) rAmount
+
+        ( Just ( lt, lb ), Just ( t, b ), Nothing ) ->
+            if b < 0.2 then
+                model
+            else
+                let
+                    lAmount =
+                        if t + b > lt + lb then
+                            0.1
+                        else
+                            0
+                in
+                    fillBucket i -(lAmount) model
+                        |> fillBucket (i - 1) lAmount
+
+        ( Nothing, Just ( t, b ), Just ( rt, rb ) ) ->
+            if b < 0.2 then
+                model
+            else
+                let
+                    rAmount =
+                        if t + b > rt + rb then
+                            0.1
+                        else
+                            0
+                in
+                    fillBucket i -(rAmount) model
+                        |> fillBucket (i + 1) rAmount
+
+        _ ->
+            model
+
+
+getHeights : Int -> Model -> Maybe ( Float, Float )
+getHeights i model =
+    case ( Array.get i model.towers, Array.get i model.buckets ) of
+        ( Just t, Just b ) ->
+            Just ( t, b )
+
+        _ ->
+            Nothing
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    every (200 * millisecond) (\_ -> Tick)
 
 
 view : Model -> Html Msg
@@ -56,6 +138,7 @@ view model =
         )
 
 
+viewSky : List (Svg Msg)
 viewSky =
     [ rect
         [ width "500"
